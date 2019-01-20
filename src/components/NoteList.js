042332a72a34e2abc43d154f86e5aa304a3491a5
Todd Kegley <todd.kegley@gmail.com>
2019-01-19 23:44:51 -0600

    back to note list

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -17,33 +17,29 @@ export default class NoteList extends React.Component {
 			<div>
 		
       <ListGroup>
-        <ListGroupItem active tag="a" href="#">
-					<div onClick={() => this.play("https://s3.amazonaws.com/study.tmp3")}>
+        <ListGroupItem   tag="a" href="#">
           	<ListGroupItemHeading> RDS Multi-AZ deployment?</ListGroupItemHeading>
           	<ListGroupItemText>
             You may observe elevated latencies..
           </ListGroupItemText>
-					</div>
-					<div>
+					
 				<ReactAudioPlayer
 					src="https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3"
 					controls
 				/>
-			</div>
+			
         </ListGroupItem>
         <ListGroupItem tag="a" href="#">
-					<div onClick={() => this.play("https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3")}>
 						<ListGroupItemHeading>List group item heading</ListGroupItemHeading>
 						<ListGroupItemText  >
 						Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
 						</ListGroupItemText>
-					</div>
 					<ReactAudioPlayer
 					src="https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3"
 					controls
 					/>
         </ListGroupItem>
-        <ListGroupItem>
+        <ListGroupItem  tag="a" href="#">
           <ListGroupItemHeading>List group item heading</ListGroupItemHeading>
           <ListGroupItemText>
           Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
