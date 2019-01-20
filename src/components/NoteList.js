960b4e2347d22db7f43f286df694178c557cc87a
Todd Kegley <todd.kegley@gmail.com>
2019-01-20 11:52:07 -0600

    filtering!

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { InputGroup, InputGroupAddon,  ListGroup, ListGroupItem, Badge, ListGroupItemHeading, ListGroupItemText, Spinner, Button, ButtonGroup , Input} from 'reactstrap';
 import ReactAudioPlayer from 'react-audio-player';
 
 export default class NoteList extends React.Component {
@@ -8,9 +8,12 @@ export default class NoteList extends React.Component {
 		this.URL = "https://xz9ki8lesa.execute-api.us-east-1.amazonaws.com/prod?postId=*";
 		// Don't call this.setState() here!
 		this.state = { 
-			notes: [],
-			isLoaded: false
+			allNotes: [],
+			notes:[],
+			isLoaded: false,
+			filterText:''
 		}
+		this.fetchNotes = this.fetchNotes.bind(this);
 	}
 	
 	componentDidMount() {
@@ -20,21 +23,63 @@ export default class NoteList extends React.Component {
 			.then(json => {
 				this.setState({
 					isLoaded: true,
-					notes: json
+					notes: json,
+					allNotes: json
 				});
 			})
-
-	
 	}
+
+	fetchNotes() {
+		console.log('entered fetch');
+		
+	}
+
+	filterTextChanged(event) {
+		console.log('filter text changed');
+		this.setState({filterText: event.target.value });
+		console.log(this.state.filterText);
+	}
+
+	clearFilter() {
+		console.log('clearfilter');
+		this.setState({
+			filterText:'',
+			notes: this.state.allNotes		
+		});
+
+	}
+	
+	applyFilter(event) {
+		console.log('apply filter entered');
+		const filteredNotes = this.state.allNotes
+			.filter( note => {
+				return note.text.substring(0, 80).toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
+			});
+		console.log(this.state.filterText);
+		console.log('filtered notes' + filteredNotes.length);
+		this.setState({notes: filteredNotes});
+	}
+
   render() {
 		let {isLoaded, notes} = this.state;
 
 			if (this.state.isLoaded ) { return (
+			<div>
+				<div>
+				<InputGroup>
+        	<InputGroupAddon addonType="prepend">Filter</InputGroupAddon>
+					<Input   placeholder="" value={this.state.filterText} onChange={evt => this.filterTextChanged(evt)} />
+      	</InputGroup>
+			 
+					<Button color="primary" onClick={() => this.applyFilter()}>Apply</Button>{' '}
+					<Button color="secondary" onClick={() => this.clearFilter()}>Clear</Button>{' '}
+					{'   '}{this.state.notes.length} Cards
+				</div>
 				<div> 
 						<ListGroup>
 							{notes.map( note => (
 								<ListGroupItem  key={note.id}>
-										<ListGroupItemHeading> {note.voice}</ListGroupItemHeading>
+										<ListGroupItemHeading tag="a" href="#" > {note.voice}</ListGroupItemHeading>
 										<ListGroupItemText>
 											{note.text.substring(0,80).concat('...')}
 										</ListGroupItemText>
@@ -47,10 +92,14 @@ export default class NoteList extends React.Component {
 							)};
 						</ListGroup>
 				</div>
+				</div>
 				); 
 			}
 			else { return   (
-				<div>Data Loading...</div>
+				<div>  
+        	<Spinner color="primary" />
+					Data Loading...
+				</div>
 				)
 			}
   }
