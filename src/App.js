aead65c2fb5e25379661332bf3541e854d98c498
Todd Kegley <todd.kegley@gmail.com>
2019-01-20 06:53:24 -0600

    remove flashcard reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import NoteList from './components/NoteList';
-import FlashCards from './components/FlashCards';
+
 
 class App extends Component {
   render() {
