01f0c5d9f244cae6629792a9a81b050a21a5d059
Todd Kegley <todd.kegley@gmail.com>
2019-01-20 08:14:06 -0600

    first increment

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,53 +5,53 @@ import ReactAudioPlayer from 'react-audio-player';
 export default class NoteList extends React.Component {
 	constructor(props) {
 		super(props);
+		this.URL = "https://xz9ki8lesa.execute-api.us-east-1.amazonaws.com/prod?postId=*";
 		// Don't call this.setState() here!
-		this.state = { counter: 0 };
-		this.play = this.play.bind(this);
+		this.state = { 
+			notes: [],
+			isLoaded: false
+		}
 	}
-	play = (noteUrl) => {
-		console.log('play the note' + noteUrl)
+	
+	componentDidMount() {
+		const url = "https://xz9ki8lesa.execute-api.us-east-1.amazonaws.com/prod?postId=*";
+		fetch(url)
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					isLoaded: true,
+					notes: json
+				});
+			})
+
+	
 	}
   render() {
-    return (
-			<div>
-		
-      <ListGroup>
-        <ListGroupItem   tag="a" href="#">
-          	<ListGroupItemHeading> RDS Multi-AZ deployment?</ListGroupItemHeading>
-          	<ListGroupItemText>
-            You may observe elevated latencies..
-          </ListGroupItemText>
-					
-				<ReactAudioPlayer
-					src="https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3"
-					controls
-				/>
-			
-        </ListGroupItem>
-        <ListGroupItem tag="a" href="#">
-						<ListGroupItemHeading>List group item heading</ListGroupItemHeading>
-						<ListGroupItemText  >
-						Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
-						</ListGroupItemText>
-					<ReactAudioPlayer
-					src="https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3"
-					controls
-					/>
-        </ListGroupItem>
-        <ListGroupItem  tag="a" href="#">
-          <ListGroupItemHeading>List group item heading</ListGroupItemHeading>
-          <ListGroupItemText>
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
-          </ListGroupItemText>
-					<ReactAudioPlayer
-					src="https://s3.amazonaws.com/study.tmksolutions-mp3/618f0e6c-6c48-433e-b23a-62ddb9b0fd01.mp3"
-					controls
-				/>
-        </ListGroupItem>
-      </ListGroup>
-			
-      </div>
-    );
+		let {isLoaded, notes} = this.state;
+
+			if (this.state.isLoaded ) { return (
+				<div> 
+						<ListGroup>
+							{notes.map( note => (
+								<ListGroupItem  key={note.id}>
+										<ListGroupItemHeading> {note.voice}</ListGroupItemHeading>
+										<ListGroupItemText>
+											{note.text.substring(0,80).concat('...')}
+										</ListGroupItemText>
+										<ReactAudioPlayer
+											src={note.url}
+											controls
+										/>
+								</ListGroupItem>
+								)
+							)};
+						</ListGroup>
+				</div>
+				); 
+			}
+			else { return   (
+				<div>Data Loading...</div>
+				)
+			}
   }
 }
